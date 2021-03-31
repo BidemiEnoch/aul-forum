75134f44329b638fa3a8f0e08b3bf0c7b4e2886a
bids416007 <33462386+bids416007@users.noreply.github.com>
2021-03-31 13:33:44 +0000

    Working on the profile page...

--- client/src/routes/profile/RightProfileSection.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const StyledRightSection = styled.div`
-    color: ${({ theme }) => theme.body.color};
-    font-size: 52px;
-    margin: auto;
-`;
-
-export default StyledRightSection;
--- client/src/routes/profile/RightProfileSection.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, ReactElement } from 'react';
-import StyledRightSection from './RightProfileSection.styled';
-
-const RightSection: FC = (): ReactElement => {
-    return <StyledRightSection>In development...</StyledRightSection>;
-};
-
-export default RightSection;
